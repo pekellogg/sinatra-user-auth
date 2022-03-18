36154b10afda81726944f3979002be7e36f3847e
Peyton <pekellogg@hotmail.com>
2022-03-17 21:00:45 -0700

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,42 +4,43 @@ class ApplicationController < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "KNx{4@q)]%4g}S9z"
   end
 
   get '/' do
-    # your code here
+    erb :home
   end
 
   get '/registrations/signup' do
-    # your code here
+    erb :'/registrations/signup'
   end
 
   post '/registrations' do
-    # your code here
-
+    @user = User.create(params)
+    session[:user_id] = @user.id
     redirect '/users/home'
   end
 
-  get '/users/home' do
-    # your code here
-  end
-
   get '/sessions/login' do
-    # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    # your code here
-
-    redirect '/users/home'
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
-    # your code here
-    
+    session.clear
     redirect '/'
   end
 
+  get '/users/home' do
+    @user = User.find(session[:user_id])
+    erb :'/users/home'
+  end
 end
